77df0ea31bc364845557865a5743f0efe6948ae0
Varun Vaibhav Jha <jha.varun6@gmail.com>
2020-12-18 19:34:19 +0530

    NoteDAO Made

---
+++ app/src/main/java/com/example/notespace/NoteDAO.kt
@@ -0,0 +1,16 @@
+package com.example.notespace
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NoteDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("SELECT * FROM notes_table ORDER BY id ASC")
+    fun getAllNotes(): LiveData<List<Note>>
+}
