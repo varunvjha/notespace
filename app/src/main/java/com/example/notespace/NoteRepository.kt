5b140d221e891b8c28ba3ce1687405fccc208b2e
Varun Vaibhav Jha <jha.varun6@gmail.com>
2020-12-18 19:51:40 +0530

    Note Repository Made

--- app/src/main/java/com/example/notespace/NoteRepository.kt
+++ app/src/main/java/com/example/notespace/NoteRepository.kt
@@ -1,5 +1,13 @@
 package com.example.notespace
 
-class NoteRepository(private val noteDAO: NoteDAO) {
+import androidx.lifecycle.LiveData
 
+class NoteRepository(private val noteDAO: NoteDAO) {
+    val allNotes: LiveData<List<Note>> = noteDAO.getAllNotes()
+    suspend fun insert(note: Note){
+        noteDAO.insert(note)
+    }
+    suspend fun delete(note: Note){
+        noteDAO.delete(note)
+    }
 }
