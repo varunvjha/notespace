87bf28eaf6ebaa93114f78183174f500eb5b1923
Varun Vaibhav Jha <jha.varun6@gmail.com>
2020-12-18 22:04:41 +0530

    RecyclerView Created

--- app/src/main/java/com/example/notespace/NoteRVAdapter.kt
+++ app/src/main/java/com/example/notespace/NoteRVAdapter.kt
@@ -1,4 +1,41 @@
 package com.example.notespace
 
-class NoteRVAdapter {
+import android.content.Context
+import android.sax.EndElementListener
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteRVAdapter(private val context:Context, private val listener: INoteRVAdapter): RecyclerView.Adapter<NoteRVAdapter.NoteViewHolder>() {
+
+    private val allNotes = ArrayList<Note>()
+
+    inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val textView: TextView = itemView.findViewById<TextView>(R.id.text)
+        val deleteButton: ImageView = itemView.findViewById<ImageView>(R.id.deleteButton)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val viewHolder = NoteViewHolder(LayoutInflater.from(context).inflate(R.layout.item_note,parent,false))
+        viewHolder.deleteButton.setOnClickListener {
+            listener.onItemClicked(allNotes[viewHolder.adapterPosition])
+        }
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote = allNotes[position]
+        holder.textView.text = currentNote.text
+    }
+
+    override fun getItemCount(): Int {
+        return allNotes.size
+    }
+}
+
+interface INoteRVAdapter{
+    fun onItemClicked(note: Note)
 }
