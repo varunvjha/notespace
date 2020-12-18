cc3e0e2b84ff2c59cd3f1eb046cf3a715a1369a2
Varun Vaibhav Jha <jha.varun6@gmail.com>
2020-12-18 19:43:33 +0530

    NoteDatabase Made

---
+++ app/src/main/java/com/example/notespace/NoteRoomDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.notespace
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], version = 1, exportSchema = false)
+abstract class NoteRoomDatabase: RoomDatabase() {
+    abstract fun noteDao(): NoteDAO
+    companion object{
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: NoteRoomDatabase? = null
+
+        fun getDatabase(context: Context): NoteRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        NoteRoomDatabase::class.java,
+                        "note_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
